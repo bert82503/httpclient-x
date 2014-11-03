0bff7fa234090c3f510207108edfd0fc04ca8ee7
huagang.li <huagang.li@fraudmetrix.cn>
2014-11-03 10:35:08 +0800

    CustomUserTokenHandler 增加实现参考引用

--- src/main/java/cn/fraudmetrix/http/impl/client/CustomUserTokenHandler.java
+++ src/main/java/cn/fraudmetrix/http/impl/client/CustomUserTokenHandler.java
@@ -5,6 +5,9 @@ import org.apache.http.protocol.HttpContext;
 
 /**
  * 类CustomUserTokenHandler.java的实现描述：自定义用户标记的{@link org.apache.http.client.UserTokenHandler UserTokenHandler}实现类。
+ * <p>
+ * 参考《HttpClient Tutorial》的<a
+ * href="http://hc.apache.org/httpcomponents-client-ga/tutorial/html/advanced.html#d5e923">7.2.1. User token handler</a>
  * 
  * @author huagang.li 2014年11月1日 下午7:05:05
  */
